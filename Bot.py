a68c50983c9689bbdc59b3efc96158071bdd45e3
enrico-pentadough <enricowilhelm25@gmail.com>
2026-04-26 19:27:36 +0800

    Still cant figure out way to bypass blocked webscrapers, also old apis used are already being blocked looking for alternatives

--- Bot.py
+++ Bot.py
@@ -31,7 +31,7 @@ async def check_stocks():
         symbol = entry["variable1"]
         buy_price = float(entry["variable2"])
 
-        stock = get_stock_price(symbol)
+        stock = await get_stock_price(symbol)
         if not stock:
             continue
 
